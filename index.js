ba38ae5e68830e28b9e3a9f5fda9066974b46bb9
Mathew Webb <matsofargone@gmail.com>
2023-04-18 21:58:02 -0700

    Created Computer choice function

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // create function that randomly selects a string r, p , s 
 
 function getComputerchoice() {
-    
+//array of computer options
+chocies = ['Rock', 'Paper', 'Scissors']
+//variable that selects a random index 0 to 2
+cpuPick = Math.floor(Math.random() * chocies.length)
+
+//returns pick 
+return chocies[cpuPick].toLowerCase()
+}
+
+
+const playerSelection = 'paper'.toLowerCase();
+const computerChoice = getComputerchoice();
+
+function playRound() {
+    //create function to play one round of r,p,s
 }
