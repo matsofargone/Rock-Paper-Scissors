e7fd268cac40384e3b58413a8414cddf3a219c8a
Mathew Webb <matsofargone@gmail.com>
2023-04-19 12:42:05 -0700

    implemented Game loop

--- index.js
+++ index.js
@@ -11,9 +11,63 @@ return chocies[cpuPick].toLowerCase()
 }
 
 
-const playerSelection = 'paper'.toLowerCase();
+const playerSelection = '';
 const computerChoice = getComputerchoice();
+let cpuScore = 0;
+let playerScore = 0;
 
-function playRound() {
+
+function playRound(playerSelection,computerMove) {
+
+  var playerChioce =  playerSelection.toLowerCase();
+ 
+
     //create function to play one round of r,p,s
+    if (playerChioce == computerMove){
+       
+        return "Tie Game!";
+    } else if (playerChioce == 'scissors' && computerMove == 'paper') {
+        playerScore ++;
+        return `Player 1 wins! \n Player Score ${playerScore} Computer Score ${cpuScore} `;
+    } else if (playerChioce == 'paper' && computerMove == 'rock') {
+        playerScore ++;
+        return `Player 1 wins! \n Player Score ${playerScore} Computer Score ${cpuScore} `;
+
+    } else if (playerChioce == 'rock' && computerMove == 'scissors'){
+        playerScore ++;
+        return `Player 1 wins! \n Player Score ${playerScore} Computer Score ${cpuScore} `;
+        
+    } else {
+        cpuScore++;
+        return `CPU wins! \n Player Score ${playerScore} Computer Score ${cpuScore} `;
+    }
+
+}
+
+// game function to handle and track score 
+
+function game() {
+    //code 
+    
+
+    while (cpuScore < 5 && playerScore < 5) {
+        var playerSelection =  prompt('Please enter Rock, Paper, Scissors');
+         
+        if (playerSelection == '') {
+            playerSelection = prompt('Valid entries: rock, paper, scissors')
+        } else {
+            console.log(playRound(playerSelection, computerChoice));
+            
+        }
+
+        
+    }
+
+    if (cpuScore > playerScore) {
+        return "Better Luck next time, CPU Wins"
+    } else {
+        return "Player gets the victory"
+    }
+
+
 }
